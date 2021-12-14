c3c7c918a4f1e3c6529c64c19b10309974a63644
Hinson Stephens <hinson@ticketsocket.com>
2021-12-14 09:10:55 -0500

    Added owner.js

---
+++ assets/owner.js
@@ -0,0 +1,15 @@
+/* 
+ * This software is copyright to Hinson Stephens of Just Biz, Inc.
+ * Please contact hinsonjr@justbiz.biz for information about the software.
+ */
+Owner = function()
+{
+	
+};
+
+Owner.Search = function(searchForm)
+{
+	
+};
+
+
